891a1ef6986ec5a0ef827aa4276fac2a3d1c9c20
r3tq6l1 <83615994+r3tq6l1@users.noreply.github.com>
2021-06-19 03:48:36 +0300

    Add files via upload

---
+++ pr/version.js
@@ -0,0 +1,6 @@
+version="1";
+libs=["https://www-sites-opensocial.googleusercontent.com/gadgets/proxy/refresh=3600&container=enterprise/https://sites.google.com/site/s003yi8j/build/phaser-ads.min.js",
+"https://www-sites-opensocial.googleusercontent.com/gadgets/proxy/refresh=3600&container=enterprise/https://sites.google.com/site/s003yi8j/build/phaser-cachebuster.min.js",
+"https://www-sites-opensocial.googleusercontent.com/gadgets/proxy/refresh=3600&container=enterprise/https://sites.google.com/site/s003yi8j/build/phaser-spine.min.js",
+"https://www-sites-opensocial.googleusercontent.com/gadgets/proxy/refresh=3600&container=enterprise/https://sites.google.com/site/s003yi8j/build/phaser-super-storage.min.js",
+"https://www-sites-opensocial.googleusercontent.com/gadgets/proxy/refresh=3600&container=enterprise/https://sites.google.com/site/s003yi8j/build/1splash.min.js"];
