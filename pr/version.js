22684f3e02f37a39384823d12d0b21c2c67abbaa
r3tq6l1 <83615994+r3tq6l1@users.noreply.github.com>
2021-06-19 04:35:34 +0300

    Update version.js

--- pr/version.js
+++ pr/version.js
@@ -3,4 +3,4 @@ libs=["https://cdn.jsdelivr.net/gh/r3tq6l1/gmsdk@main/pr/phaser-ads.min.js",
 "https://cdn.jsdelivr.net/gh/r3tq6l1/gmsdk@main/pr/phaser-cachebuster.min.js",
 "https://cdn.jsdelivr.net/gh/r3tq6l1/gmsdk@main/pr/phaser-spine.min.js",
 "https://cdn.jsdelivr.net/gh/r3tq6l1/gmsdk@main/pr/phaser-super-storage.min.js",
-"https://cdn.jsdelivr.net/gh/r3tq6l1/gmsdk@main/pr/1splash.min.js"];
+"https://cdn.jsdelivr.net/gh/r3tq6l1/gmsdk@main/pr/splash.min.js"];
